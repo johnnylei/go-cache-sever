7f45a6445ac7a7d0465767655cf517f0f7fc61b0
leiyuqing02 <leiqyuing_jing@163.com>
2020-01-24 20:58:42 +0800

    init

---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+type Stat struct {
+	Count int64 `json:"count"`
+	KeySize int64 `json:"key_size"`
+	ValueSize int64 `json:"value_size"`
+}
+
+func (_self *Stat)add(key string, value []byte)  {
+	_self.Count++
+	_self.KeySize += int64(len(key))
+	_self.ValueSize += int64(len(value))
+}
+
+func (_self *Stat)del(key string, value []byte)  {
+	_self.Count--
+	_self.KeySize -= int64(len(key))
+	_self.ValueSize -= int64(len(value))
+}
+
+type Cache struct {
+	*Stat
+	Data map[string][]byte
+	Lock sync.RWMutex
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		&Stat{},
+		make(map[string][]byte),
+		sync.RWMutex{},
+	}
+}
+
+func (_self *Cache)Set(key string, value []byte) error  {
+	key = strings.Trim(key, " ")
+	if len(key) == 0 {
+		return errors.New("key should not be empty")
+	}
+
+	_self.Lock.Lock()
+	defer _self.Lock.Unlock()
+	if _value, okay := _self.Data[key]; okay {
+		_self.del(key, _value)
+	}
+
+	_self.Data[key] = value
+	_self.add(key, value)
+	return nil
+}
+
+func (_self *Cache)Get(key string) ([]byte, error)  {
+	key = strings.Trim(key, " ")
+	if len(key) == 0 {
+		return nil, errors.New("key should not be empty")
+	}
+
+	_self.Lock.RLock()
+	defer _self.Lock.RUnlock()
+	return _self.Data[key], nil
+}
+
+func (_self *Cache)Del(key string) error  {
+	key = strings.Trim(key, " ")
+	if len(key) == 0 {
+		return errors.New("key should not be empty")
+	}
+	
+	_self.Lock.Lock()
+	defer _self.Lock.Unlock()
+	if value, okay := _self.Data[key]; okay {
+		_self.del(key, value)
+		delete(_self.Data, key)
+	}
+
+	return nil
+}
+
+type CacheHandler struct {
+	cache *Cache
+}
+
+func (_self *CacheHandler)ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	key := strings.Split(r.URL.EscapedPath(), "/")[2]
+	if len(key) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	method := r.Method
+	if method == http.MethodPut {
+		value, _ := ioutil.ReadAll(r.Body)
+		if len(value) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if err := _self.cache.Set(key, []byte(value)); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte("succeed"))
+	} else if method == http.MethodGet {
+		value, err := _self.cache.Get(key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(value)
+	} else if method == http.MethodDelete {
+		err := _self.cache.Del(key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte("succeed"))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+type StatusHandler struct {
+	cache *Cache
+}
+
+func (_self *StatusHandler)ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	status,_ := json.Marshal(_self.cache.Stat)
+	w.Write(status)
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	cache := NewCache()
+	http.Handle("/cache/", &CacheHandler{
+		cache:cache,
+	})
+	http.Handle("/status", &StatusHandler{
+		cache:cache,
+	})
+	http.ListenAndServe("0.0.0.0:12345", nil)
+}
